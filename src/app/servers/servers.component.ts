77aa0da06d34fda370d56fdd4f6b94afbf5d1e72
Kathryn <kdboudreau1@gmail.com>
2018-04-25 16:33:45 -0400

    adding string lesson

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server>',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  // template: '<app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = "No server was created";
+  constructor() {
+      setTimeout(() => {
+        this.allowNewServer = true;
+      }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+      this.serverCreationStatus = "Server was created";
   }
 
 }
