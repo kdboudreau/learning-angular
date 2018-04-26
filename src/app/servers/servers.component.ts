88474d5da563b467ff09ac1e8017f6f0358fc4be
Kathryn <kdboudreau1@gmail.com>
2018-04-26 16:05:32 -0400

    finished covering all 4 ways to data bind

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created";
+  serverName = "";
+  serverCreated = false;
   constructor() {
       setTimeout(() => {
         this.allowNewServer = true;
@@ -21,7 +23,12 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-      this.serverCreationStatus = "Server was created";
+    this.serverCreated = true;
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
